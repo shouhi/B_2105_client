19ccfa3aa51dfb61638d5052a86e6404b5781aaf
ide Shouhi <shouhi0922@icloud.com>
2021-10-23 12:30:34 +0900

    :zap: add-user_model

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,22 @@
+export type UserType = {
+  id: string;
+  name: string;
+  avatarUrl: string;
+  belongs: string;
+  role: string;
+  github: string;
+  twitter: string;
+  instagram: string;
+  discription: string;
+};
+
+export type UserPutRequest =
+  | UserType
+  | Pick<UserType, "id" | "name">
+  | Pick<UserType, "id" | "avatarUrl">
+  | Pick<UserType, "id" | "belongs">
+  | Pick<UserType, "id" | "role">
+  | Pick<UserType, "id" | "github">
+  | Pick<UserType, "id" | "twitter">
+  | Pick<UserType, "id" | "instagram">
+  | Pick<UserType, "id" | "discription">;
