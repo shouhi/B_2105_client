ce85a416489bdcd48449228c43339070b0ef8470
shouhi <shouhi605@gmail.com>
2021-10-23 16:35:30 +0900

    :sparkles: add HomePage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,45 +1,27 @@
 import { Layout } from "../components/shared/Layout";
 import { Button } from "../components/shared/Button";
-import { EXAMPLE_USER_01 } from "../models/user";
+import Image from "next/image";
+import Link from "next/link";
+import { NextPage } from "next";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <Layout
-      left="icon"
-      right={[
-        <Button
-          key="write memo"
-          variant="solid-blue"
-          linkProps={{ href: "/memos/new" }}
-          className="px-4 h-10"
-        >
-          面接練習
-        </Button>,
-      ]}
-    >
-      <div className="p-10 bg-gray-100">
-        <div className="rounded-xl max-w-5xl overflow-hidden shadow-lg bg-gray-50 py-5">
-          <div className="px-6 py-4">
-            <div className="font-bold text-2xl mb-2">
-              🦔 Hello ! {EXAMPLE_USER_01.name} さん
-            </div>
-            <p className="text-gray-700 text-base">
-              {EXAMPLE_USER_01.discription}
-            </p>
-          </div>
-          <div className="px-6 pt-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #TechUni
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #アプリ開発
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #機械学習
-            </span>
-          </div>
+    <div className="w-screen h-screen bg-white font-family-karla">
+      <div className="grid place-items-center mt-12 md:pt-0 md:px-24 lg:px-32">
+        <Image src="/icon.png" width={250} height={200} />
+        <p className="text-center text-xl font-bold mt-6">面接Tech</p>
+        <div className="pt-6 pb-12">
+          <p className="text-lm">
+            今すぐ面接練習してみませんか？{" "}
+            <Link href="/dashboard">
+              <a className="text-blue-400">はじめる</a>
+            </Link>
+            <Image src="/mock/mockup.png" width={500} height={400} />
+          </p>
         </div>
       </div>
-    </Layout>
+    </div>
   );
-}
+};
+
+export default Home;
