ff50234b20f74301b4d8d0f87469abff19fadd76
ide Shouhi <shouhi0922@icloud.com>
2021-10-23 12:29:50 +0900

    :sparkles: add-header(Test)

---
+++ frontend/src/components/shared/Layout.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode, VFC } from "react";
+import type { HeaderProps } from "../layouts/Header";
+import { Header } from "../layouts/Header";
+
+type Props = HeaderProps & {
+  children: ReactNode;
+  // isHeaderNarrow?: boolean;
+};
+
+export const Layout: VFC<Props> = (props) => {
+  const { children, ...headerProps } = props;
+
+  return (
+    <div className="pb-20">
+      <div className="pt-4 pb-1 mx-auto">
+        <Header {...headerProps} />
+      </div>
+      <div className="flex min-h-screen w-screen">
+        <main className="w-full bg-gray-50">{children}</main>
+      </div>
+      <footer>
+        <div className="p-8">
+          <div className="text-gray-600 text-center">
+            <p>Tech.Uni org.</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
