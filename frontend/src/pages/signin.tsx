4c240c46f2551cf47b7f30dbff37391685b67bae
shouhi <shouhi605@gmail.com>
2021-10-24 11:48:05 +0900

    :sparkles: signIn && signup

---
+++ frontend/src/pages/signin.tsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect } from "react";
+import type { NextPage } from "next";
+import { AuthContext } from "../components/auth/AuthProvider";
+import { useRouter } from "next/router";
+import { Button } from "../components/shared/Button";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubIcon } from "../components/icon/GithubIcon";
+
+import { githubProvider } from "../components/auth/AuthMethods";
+import socialMediaAuth from "../components/auth/SocialMediaAuth";
+
+const Signin: NextPage = () => {
+  const router = useRouter();
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    currentUser && router.push("/");
+  }, [currentUser]);
+
+  const handleOnClick = async (provider) => {
+    const res = await socialMediaAuth(provider);
+    console.log(res);
+  };
+
+  return (
+    <div className="w-screen h-screen bg-white font-family-karla">
+      <div className="grid place-items-center mt-12 md:pt-0 md:px-24 lg:px-32">
+        <Image src="/icon.png" width={250} height={200} />
+        <p className="text-center text-xl font-bold mt-6">
+          アカウントにログイン
+        </p>
+        <Button
+          variant="solid-black"
+          className="py-4 w-72 sm:w-80 mt-7"
+          onClick={() => {
+            handleOnClick(githubProvider);
+          }}
+        >
+          <div className="flex">
+            <GithubIcon iconColor="white" className="mr-3" />
+            <span>Sign in with Github</span>
+          </div>
+        </Button>
+        <div className=" pt-6 pb-12">
+          <p className="text-sm">
+            アカウントをお持ちでありませんか？{" "}
+            <Link href="/signup">
+              <a className="text-blue-400">サインアップ</a>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Signin;
