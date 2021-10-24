b586629e754c12366ba4893942c50ea06b4210c6
shouhi <shouhi605@gmail.com>
2021-10-24 11:47:14 +0900

    :sparkles: provider<auth>

---
+++ frontend/src/components/auth/SocialMediaAuth.tsx
@@ -0,0 +1,14 @@
+import { auth } from "../../utils/firebase";
+
+const socialMediaAuth = (provider) => {
+  auth
+    .signInWithPopup(provider)
+    .then((res) => {
+      return res.user;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export default socialMediaAuth;
