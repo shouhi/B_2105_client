f52e2f0b95f58c291171a3a667517cb24f0dae1b
GitHayato <gabayt0_0@icloud.com>
2021-10-29 00:16:32 +0900

    :rotating_light: fix lint error

--- frontend/src/pages/test/index.tsx
+++ frontend/src/pages/test/index.tsx
@@ -7,49 +7,48 @@ import { Button } from '../../components/shared/Button'
 import { Layout } from '../../components/shared/Layout'
 
 const Test: NextPage = () => {
-  const webcamRef = useRef(null);
-  const mediaRecorderRef = useRef(null);
-  const [capturing, setCapturing] = useState(false);
-  const [recordedChunks, setRecordedChunks] = useState([]);
-
-  const handleStartCaptureClick = useCallback(() => {
-    setCapturing(true);
-    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-      mimeType: "video/webm"
-    });
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
-    mediaRecorderRef.current.start();
-  }, [webcamRef, setCapturing, mediaRecorderRef]);
+  const webcamRef = useRef(null)
+  const mediaRecorderRef = useRef(null)
+  const [capturing, setCapturing] = useState(false)
+  const [recordedChunks, setRecordedChunks] = useState([])
 
   const handleDataAvailable = useCallback(
     ({ data }) => {
       if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
+        setRecordedChunks(prev => prev.concat(data))
       }
     },
     [setRecordedChunks]
-  );
+  )
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true)
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: 'video/webm',
+    })
+    mediaRecorderRef.current.addEventListener(
+      'dataavailable',
+      handleDataAvailable
+    )
+    mediaRecorderRef.current.start()
+  }, [webcamRef, setCapturing, mediaRecorderRef])
 
   const handleStopCaptureClick = useCallback(() => {
-    mediaRecorderRef.current.stop();
-    setCapturing(false);
-  }, [mediaRecorderRef, webcamRef, setCapturing]);
+    mediaRecorderRef.current.stop()
+    setCapturing(false)
+  }, [mediaRecorderRef, webcamRef, setCapturing])
 
   const handleDownload = useCallback(() => {
     if (recordedChunks.length) {
       const blob = new Blob(recordedChunks, {
-        type: "video/webm"
-      });
+        type: 'video/webm',
+      })
       const url = URL.createObjectURL(blob)
       console.log(url)
       window.URL.revokeObjectURL(url)
-      setRecordedChunks([]);
+      setRecordedChunks([])
     }
-  }, [recordedChunks]);
-
+  }, [recordedChunks])
 
   return (
     <Layout
@@ -69,9 +68,21 @@ const Test: NextPage = () => {
         <div className="rounded-xl max-w-5xl  container mx-auto overflow-hidden shadow-lg bg-gray-50 p-10">
           <Webcam audio={false} ref={webcamRef} className="w-full" />
           {capturing ? (
-            <Button variant="solid-red" onClick={handleStopCaptureClick} className="px-4 h-10">Stop Capture</Button>
+            <Button
+              variant="solid-red"
+              onClick={handleStopCaptureClick}
+              className="px-4 h-10"
+            >
+              Stop Capture
+            </Button>
           ) : (
-            <Button variant="solid-blue" onClick={handleStartCaptureClick} className="px-4 h-10">開始する</Button>
+            <Button
+              variant="solid-blue"
+              onClick={handleStartCaptureClick}
+              className="px-4 h-10"
+            >
+              開始する
+            </Button>
           )}
           {recordedChunks.length > 0 && (
             <Button onClick={handleDownload}>download</Button>
