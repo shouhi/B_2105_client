2b14979c97593cfaa8c1eae507d509555c764936
shouhi <shouhi605@gmail.com>
2021-10-30 07:02:54 +0900

    :lipstick: display

--- frontend/src/pages/result/index.tsx
+++ frontend/src/pages/result/index.tsx
@@ -1,6 +1,15 @@
+import firebase from 'firebase/compat/app'
+import {
+  collection,
+  FirestoreDataConverter,
+  getFirestore,
+  QueryDocumentSnapshot,
+  WithFieldValue,
+} from 'firebase/firestore'
 import type { NextPage } from 'next'
 // import { useRouter } from 'next/router'
 // import { useContext } from 'react'
+
 import {
   Radar,
   RadarChart,
@@ -14,19 +23,18 @@ import {
 import { Layout } from '../../components/shared/Layout'
 
 const radardata = [
-  { subject: 'anger', A: 120, B: 110, fullMark: 150 },
-  { subject: 'surprise', A: 98, B: 130, fullMark: 150 },
-  { subject: 'disgust', A: 86, B: 130, fullMark: 150 },
-  { subject: 'sadness', A: 99, B: 100, fullMark: 150 },
-  { subject: 'contempt', A: 85, B: 90, fullMark: 150 },
-  { subject: 'fear', A: 65, B: 85, fullMark: 150 },
+  { feel: 'anger', A: 120, B: 110, fullMark: 150 }, //A = 自分   B = 平均
+  { feel: 'surprise', A: 98, B: 130, fullMark: 150 },
+  { feel: 'disgust', A: 86, B: 130, fullMark: 150 },
+  { feel: 'sadness', A: 99, B: 100, fullMark: 150 },
+  { feel: 'contempt', A: 85, B: 90, fullMark: 150 },
+  { feel: 'fear', A: 65, B: 85, fullMark: 150 },
 ]
 
 const Result: NextPage = () => {
   // const { push } = useRouter()
 
   // const { currentUser } = useContext(AuthContext)
-
   return (
     <Layout left="icon" right={['profile']}>
       <div className="p-5 bg-gray-100">
@@ -38,31 +46,22 @@ const Result: NextPage = () => {
             <RadarChart
               cx={300} // 要素の左端とチャートの中心点との距離(0にするとチャートの左半分が隠れる)
               cy={300} // 要素の上部とチャートの中心点との距離(0にするとチャートの上半分が隠れる)
-              outerRadius={250} // レーダーチャート全体の大きさ
-              width={600} // レーダーチャートが記載される幅(この幅よりチャートが大きい場合、はみ出た箇所は表示されない)
-              height={600} // レーダーチャートが記載される高さ
-              data={radardata} // 表示対象のデータ
+              outerRadius={250}
+              width={600}
+              height={600}
+              data={radardata}
             >
-              {/* レーダーチャートの蜘蛛の巣のような線 */}
               <PolarGrid />
-              {/* 項目を決めるデータのキー(サンプルでいう数学や歴史) */}
-              <PolarAngleAxis dataKey="subject" />
+              <PolarAngleAxis dataKey="feel" />
 
-              {/* 目安となる数値が表示される線を指定  */}
-              <PolarRadiusAxis
-                angle={30} // 中心点から水平を0°とした時の角度 垂直にしたいなら90を指定
-                domain={[0, 150]} // リストの１番目の要素が最小値、2番目の要素が最大値
-              />
-
-              {/* レーダーを表示 */}
+              <PolarRadiusAxis angle={30} domain={[0, 150]} />
               <Radar
-                name="あなた" // そのチャートが誰のデータか指定(チャート下にここで指定した値が表示される)
-                dataKey="A" // 表示する値と対応するdata内のキー
+                name="あなた"
+                dataKey="A"
                 stroke="#8884d8" // レーダーの外枠の色
                 fill="#8884d8" // レーダー内の色
                 fillOpacity={0.5} // レーダー内の色の濃さ(1にすると濃さMAX)
               />
-              {/* ２個目のレーダー */}
               <Radar
                 name="全国の平均"
                 dataKey="B"
