00c478350cc74251ceb85ba8107ee9002f31b10a
GitHayato <gabayt0_0@icloud.com>
2021-10-29 16:51:54 +0900

    :sparkles: display sample questions

--- frontend/src/pages/test/index.tsx
+++ frontend/src/pages/test/index.tsx
@@ -1,15 +1,43 @@
 import type { NextPage } from 'next'
+// import { useRouter } from 'next/router'
 
-import { useRef, useState, useCallback } from 'react'
+import { useRef, useState, useCallback, useEffect } from 'react'
 import Webcam from 'react-webcam'
 
+import { Button } from '../../components/shared/Button'
 import { Layout } from '../../components/shared/Layout'
+import { EXAMPLE_QUESTIONS } from '../../models/questions'
+import { QuestionType } from '../../types/types'
 
 const Test: NextPage = () => {
   const webcamRef = useRef(null)
   const mediaRecorderRef = useRef(null)
   const [capturing, setCapturing] = useState(false)
   const [recordedChunks, setRecordedChunks] = useState([])
+  const [interviewQuestions, setInterviewQuestions] = useState<QuestionType[]>([])
+  const [questionId, setQuestionId] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState<QuestionType>()
+
+  // const { query } = useRouter()
+
+  // 配列内の要素の順番をシャッフル（フィッシャー・イェーツのランダムソート）
+  // const shuffle = ([...array]) => {
+  //   for(let i = array.length - 1; i > 0; i--) {
+  //       const j = Math.floor(Math.random() * (i + 1))
+  //       const tmp = array[i]
+  //       array[i] = array[j]
+  //       array[j] = tmp
+  //   }
+  //   return array
+  // }
+
+  useEffect(() => {
+    // if (query.id === 'practice') {
+    //   const questions = shuffle(EXAMPLE_QUESTIONS)
+    //   setInterviewQuestions(questions)
+    // }
+    setInterviewQuestions(EXAMPLE_QUESTIONS)
+  }, [interviewQuestions])
 
   const handleDataAvailable = useCallback(
     ({ data }) => {
@@ -49,19 +77,48 @@ const Test: NextPage = () => {
     }
   }, [recordedChunks])
 
+  const handleClickNextQuestion = () => {
+    const nextId = questionId + 1
+    setQuestionId(nextId)
+  }
+
+  useEffect(() => {
+    setCurrentQuestion(interviewQuestions[questionId])
+  }, [questionId, interviewQuestions, currentQuestion])
+
   return (
     <Layout left="icon" right={['profile']}>
-      <div className="p-10 bg-gray-100">
-        <div className="rounded-xl max-w-5xl  container mx-auto overflow-hidden shadow-lg bg-gray-50 p-10">
+      <div className="p-3 bg-gray-100">
+        <div className="rounded-xl max-w-5xl container mx-auto overflow-hidden shadow-lg bg-gray-50 p-10">
+          <div className="">
+            {capturing ? (
+              <div>
+                <p>{currentQuestion.question}</p>
+              </div>
+            ) : (
+              <p className="font-md">開始するを押すと、面接が始まります</p>
+            )}
+          </div>
           <Webcam audio={false} ref={webcamRef} className="w-full" />
           {capturing ? (
-            <Button
-              variant="solid-red"
-              onClick={handleStopCaptureClick}
-              className="px-4 h-10"
-            >
-              Stop Capture
-            </Button>
+            <>
+              {questionId < interviewQuestions.length - 1 && (
+                  <Button
+                    variant="solid-blue"
+                    onClick={handleClickNextQuestion}
+                    className="px-4 h-10"
+                  >
+                    Next Question
+                  </Button>
+              )}
+              <Button
+                variant="solid-red"
+                onClick={handleStopCaptureClick}
+                className="px-4 h-10"
+              >
+                Stop Capture
+              </Button>
+            </>
           ) : (
             <Button
               variant="solid-blue"
